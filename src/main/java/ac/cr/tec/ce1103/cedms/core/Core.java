8c14306f406ffb1c4e8b0664dd970027fa985af0
pablo <pablo@gmail.com>
2014-11-10 12:21:30 -0600

    atributos core

--- src/main/java/ac/cr/tec/ce1103/cedms/core/Core.java
+++ src/main/java/ac/cr/tec/ce1103/cedms/core/Core.java
@@ -1,8 +1,15 @@
 package ac.cr.tec.ce1103.cedms.core;
 
+import ac.cr.tec.ce1103.cedms.dataStructures.Queue;
+
+import java.net.Socket;
+
 /**
  * Created by pablo on 10/11/14.
  */
 public abstract class Core {
+    private Socket socket;
+    private Queue queueIn;
+    private Queue queueOut;
 
 }
