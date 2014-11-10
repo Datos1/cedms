c76a6811aed074b5c78402460eb3095ac4ceacd1
pablo <pablo@gmail.com>
2014-11-09 18:02:24 -0600

    Initial commit

--- src/main/java/ac/cr/tec/ce1103/cedms/App.java
+++ src/main/java/ac/cr/tec/ce1103/cedms/App.java
@@ -1,13 +1,10 @@
 package ac.cr.tec.ce1103.cedms;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
     }
+
+
 }
