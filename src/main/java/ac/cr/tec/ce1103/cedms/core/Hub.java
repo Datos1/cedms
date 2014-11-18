8aad25b8e415c534d3797b9e56abe24172a81c33
pablo <pablo@gmail.com>
2014-11-18 12:52:37 -0600

    HUB

--- src/main/java/ac/cr/tec/ce1103/cedms/core/Hub.java
+++ src/main/java/ac/cr/tec/ce1103/cedms/core/Hub.java
@@ -3,5 +3,33 @@ package ac.cr.tec.ce1103.cedms.core;
 /**
  * Created by pablo on 10/11/14.
  */
-public class Hub {
+public class Hub extends Core {
+    public Hub(int pId, int pPort) {
+        super(pId, pPort);
+    }
+
+    @Override
+    public void recibirConnection(int source, int target, int id, int adyacente, int precio, int updateId) {
+
+    }
+
+    /**
+     * It diffuses a message through all the system
+     *
+     * @param msg
+     */
+    @Override
+    public void difusion(String msg) {
+
+    }
+
+    @Override
+    protected void initScanner() {
+
+    }
+
+    @Override
+    protected void createConnection() {
+
+    }
 }
