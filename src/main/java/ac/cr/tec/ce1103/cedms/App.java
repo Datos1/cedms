efae25f38d4b7f9e601adcc7b0fe0af1d7ae499e
paroque28 <paroque28@gmail.com>
2014-11-19 14:28:54 -0600

    Update App.java
    Se elimino las pruebas de graph

--- src/main/java/ac/cr/tec/ce1103/cedms/App.java
+++ src/main/java/ac/cr/tec/ce1103/cedms/App.java
@@ -13,42 +13,9 @@ public class App {
     public static final String CLIENT = "client";
     public static final String HUB = "hub";
     public static final String BASE_STATION = "base-station";
-    public static int INSTANCE_COUNTER=1;// instance counter for the unique graph node id.
-    // each time a grapNode os created, this variable increments 1 value manually.
 
 
     public static void main(String[] args) {
-        Graph graph=new Graph();
-        BaseStation base1=new BaseStation(123456789,5012);
-       BaseStation base2=new BaseStation(1234567,5012);
-        Hub hub1=new Hub(123456,5012);
-        Client client1= new Client(1239,5012);
-        graphNode nodo1=new graphNode(base1);
-        graphNode nodo2=new graphNode(hub1);
-        graphNode nodo3=new graphNode(client1);
-        graphNode nodo4=new graphNode(base2);
-        INSTANCE_COUNTER+=1;//contador para el id de cada instancia, ya sea hub, client o base station.
-        nodo4.setId();
-        graph.addNode(nodo1);
-
-      //  System.out.println(nodo1.getId());
-        graph.addNode(nodo2);
-      //  System.out.println(nodo2.getId());
-        graph.addNode(nodo3);
-       // System.out.println(nodo3.getId());
-        graph.addNode(nodo4);
-      //  System.out.println(nodo4.getId());
-      //  System.out.println(((graphNode)(graph.getGraph_nodes().get(1))).getNombre_elemento());
-        for(int i=0;i<graph.getNum_nodes();i++){
-            System.out.println(((graphNode)(graph.getGraph_nodes().get(i))).getNombre_elemento());
-        }
-
-
-
-       // graph.getAllConnections();
-        //graph.printAristas();
-
-       /* Core core;
         if (args.length != 3)
             System.out.println(USAGE);
         try {
@@ -66,6 +33,6 @@ public class App {
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(USAGE);
         }
-    }*/
+    }
 
 }}
