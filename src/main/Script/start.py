f393fc81e44a90e4d0211e527ac0239ca8f8709b
briam <briamtec@gmail.com>
2014-11-23 05:06:22 -0600

    python script

--- src/main/Script/start.py
+++ src/main/Script/start.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+from subprocess import call
+from xml.dom import minidom
 
-def hello_command(name, print_counter=False, repeat=10):
-        """Print nice greetings."""
-        for i in range(repeat):
-            if print_counter:
-                print i+1,
-            print 'Hello, %s!' % name
+def start():
+        """Correr la progra"""
+        fo = open("grafo.xml","r+")
+        doc = minidom.parse(fo)
+        conexiones = doc.getElementsByTagName("conexion")
+        dispositivos= doc.getElementsByTagName("dispositivo")
+        for dispositivo in dispositivos:
+            id= (dispositivo.getAttribute("id"))
+            tipo= dispositivo.getAttribute("tipo")
+            puerto= (dispositivo.getAttribute("puerto"))
+            print "java -jar ~/cedms.jar "+id+" "+puerto+" "+tipo
+            call(["xterm", '-e', "java -jar ~/cedms.jar "+id+" "+puerto+" "+tipo])#abre una consola con el comando indicado
+
 if __name__ == '__main__':
-    import scriptine
-    scriptine.run()
+    start()
